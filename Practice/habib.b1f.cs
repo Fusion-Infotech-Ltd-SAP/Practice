ee944bcbe4e775d42d3399868c48b9bfe934f1e3
Fusion Infotech <fusioninfotech640@gmail.com>
2026-06-17 12:58:18 +0600

    feat: add new form called habib

---
+++ Practice/habib.b1f.cs
@@ -0,0 +1,40 @@
+﻿using SAPbouiCOM.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice
+{
+    [FormAttribute("Practice.habib", "habib.b1f")]
+    class habib : UserFormBase
+    {
+        public habib()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_0").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.StaticText StaticText0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+    }
+}
